789659344a9d8d466ad2ceea231a415d52827a4e
Chris <chris@chris-laptop.(none)>
2009-05-31 18:18:18 -0600

    working on members show and links

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,5 @@
 class Link < ActiveRecord::Base
   belongs_to :member, :dependent =>  :destroy
+   attr_accessible :url, :my_type
   MY_TYPES = ["Facebook", "Twitter", "Smibs", "Personal", "Work", "Other"]
 end
